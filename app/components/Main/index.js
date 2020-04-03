5ea278580f5d2b9188b80481fc72c0b9ff68f615
Jozdien <joseverres42@gmail.com>
2020-04-03 17:23:48 +0530

    First

---
+++ app/components/Main/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Linking, ScrollView} from 'react-native';
+import styles from './styles';
+
+export default class La2013 extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+    }
+  }
+  static navigationOptions={
+    header: null  
+  }
+  render(){ 
+    return(
+      <View style={styles.parent}>
+      
+      </View>
+    );
+  }
+}
